4cebdebf05f3a6c529931cac475d13641cbfbf02
swatisaha <contact@swatisaha.com>
2018-11-30 12:38:24 +0530

    my first commit

---
+++ ledblink.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT, initial=GPIO.LOW)
+
+while True:
+    GPIO.output(11, GPIO.HIGH)
+    sleep(5)
+    GPIO.output(11, GPIO.LOW)
+    sleep(5)
+
+
